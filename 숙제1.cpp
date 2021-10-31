c6cf7fc4d55fa773395495c3f34244932782a6ac
kiption <seichulee@naver.com>
2021-10-31 23:52:36 +0900

    #실습21 완료, 숙제1 미로남음

--- 숙제1.cpp
+++ 숙제1.cpp
@@ -1,5 +1,3 @@
-#include "readObj.h"
-#include <vector>
 #include <iostream>
 #include <gl/glew.h>
 #include <gl/freeglut.h>
@@ -8,11 +6,15 @@
 #include <Windows.h>
 #include <fstream>
 #include <sstream>
+#include <vector>
 #include <string>
 #include <queue>
 #include <glm/glm/glm.hpp>
 #include <glm/glm/ext.hpp>
 #include <glm/glm/gtc/matrix_transform.hpp>
+#include "readObj.h"
+#define COL 25
+#define ROW 25
 
 using namespace std;
 
@@ -30,56 +32,70 @@ GLvoid drawscene(GLvoid);
 GLvoid reshape(int w, int h);
 GLvoid keyboard(unsigned char key, int x, int y);
 
-void randomExistSquare(int i);
+void randomScale(int i, int j);
+void makeModelColor(int i, int j);
 void drawmodels();
 
 float CameraTheta = 0;
-float ObjectTheta = 0;
+float ObjectThetaX = -20.0f;
+float ObjectThetaY = 0.0f;
+bool OnceMakeColor = true;
+bool MoutainScaleOn = false;
 
-bool randomExist = true;
-bool smallscale[30] = { 0 };
-
-int ChangeSmallC = -1;
-
-int ModelNumber = 30;
 int windowWidth, windowHeight;
+int ArmRotateVector = 1;
 
-struct PrimeScale {
-    float x = 0.25f;
-    float y = 0.25f;
-    float z = 0.25f;
-};
+int ModelCountCol = 0;
+int ModelCountRow = 0;
 
-PrimeScale ChangeScaleSquqre[30];
+float ModelColorR[COL][ROW];
+float ModelColorG[COL][ROW];
+float ModelColorB[COL][ROW];
+
+float MoutainScale[COL][ROW];
+int ScaleCount[COL][ROW];
+
+bool ViewTransX = false;
+bool ViewTransY = false;
+
+struct Modelcolor {
+    float modelR = 0.0f;
+    float modelB = 0.0f;
+    float modelG = 0.0f;
+}ModelColorSize;
 
 struct CameraViewPoint {
     float ViewX = 0.0f;
-    float ViewY = 1.0f;
-    float ViewZ = 5.0f;
+    float ViewY = 0.0f;
+    float ViewZ = 6.0f;
 }CameraPoint;
 
-struct EraseTrans {
+struct CrainTrans {
     float AllTransX = 0.0f;
     float AllTransY = 0.0f;
     float AllTransZ = 0.0f;
-}EraserTransSize;
+}CrainTransSize;
 
-vector<glm::vec4>ModelsValue[33];
+vector<glm::vec4>ModelsValue[2];
 
-GLuint VAO[33], VBO[33];
+GLuint VAO[2], VBO[2];
 GLuint shaderID[2];
 GLuint qobjshader;
 GLuint vertexshader;
 GLuint fragmentshader;
 GLuint triangleshaderProgram;
 
-float ModelTransX[30];
-float ModelTransZ[30];
-
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
+    cout << "Write the size of the square you want to make." << endl;
+    cin >> ModelCountCol;
+    cin >> ModelCountRow;
+    if (ModelCountCol < 5 || ModelCountCol>25 || ModelCountRow < 5 || ModelCountRow>25) {
+        cin >> ModelCountCol;
+        cin >> ModelCountRow;
+    }
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-    glutInitWindowPosition(0, 0);
+    glutInitWindowPosition(400, 300);
     glutInitWindowSize(800, 600);
     glutCreateWindow("example3");
 
@@ -92,18 +108,11 @@ int main(int argc, char** argv) {
         cout << "GLEW initialized" << endl;
     }
     //obj 파일읽기
-    
+   
+
     ReadObj("plane.obj", ModelsValue[0]);
     ReadObj("crainCube.obj", ModelsValue[1]);
-    ReadObj("crainArm.obj", ModelsValue[2]);
-    
-    
 
-    if (randomExist) {
-		for (int ModelCountNumber = 0; ModelCountNumber < ModelNumber; ModelCountNumber++) {
-			ReadObj("crainCube.obj", ModelsValue[ModelCountNumber + 3]);
-		}
-    }
     make_vertexShaders();
     make_fragmentShaders();
     initbuffer();
@@ -211,98 +220,69 @@ GLuint make_shaderProgram() {
 
     return 1;
 }
-
-void randomExistSquare(int i) {
-    random_device rd;
-    default_random_engine dre(rd());
-    uniform_real_distribution<> urd(-2.5, 2.5);
-    
-    float randomX = urd(dre);
-    float randomZ = urd(dre);
-
-    ModelTransX[i] = randomX;
-    ModelTransZ[i] = randomZ;
-}
-
 void drawmodels() {
     //바닥면
+
+    glDisable(GL_CULL_FACE);
     glBindVertexArray(VAO[0]);
     glm::mat4 PlaneModel = glm::mat4(1.0f);
-    PlaneModel = glm::scale(PlaneModel, glm::vec3(6.0f, 0, 6.0f));
+    PlaneModel = glm::translate(PlaneModel, glm::vec3(0.0f, -0.02f, 0.0f));
+    PlaneModel = glm::scale(PlaneModel, glm::vec3(ModelCountCol*0.5f, 1.0f, ModelCountRow * 0.5f));
     unsigned int PlaneModelLocation = glGetUniformLocation(shaderID[1], "modeltransform");
     glUniformMatrix4fv(PlaneModelLocation, 1, GL_FALSE, glm::value_ptr(PlaneModel));
     unsigned int PlaneModelFragLocation = glGetUniformLocation(shaderID[1], "vColor");
     glUniform3f(PlaneModelFragLocation, 0.5f, 0.75f, 0.27f);
     glDrawArrays(GL_QUADS, 0, ModelsValue[0].size());
-
-    //지우개 몸통
-    glBindVertexArray(VAO[1]);
-    glm::mat4 BodyModel = glm::mat4(1.0f);
-    BodyModel = glm::translate(BodyModel, glm::vec3(EraserTransSize.AllTransX, EraserTransSize.AllTransY, EraserTransSize.AllTransZ));
-    BodyModel = glm::scale(BodyModel, glm::vec3(0.5f,0.5f,0.5f));
     
-    unsigned int BodyModelLocation = glGetUniformLocation(shaderID[1], "modeltransform");
-    glUniformMatrix4fv(BodyModelLocation, 1, GL_FALSE, glm::value_ptr(BodyModel));
-    unsigned int BodyModelFragLocation = glGetUniformLocation(shaderID[1], "vColor");
-    glUniform3f(BodyModelFragLocation, 1.0f, 0.2f, 0.6f);
-    glDrawArrays(GL_QUADS, 0, 24);
-
-    float BodybottomMinx = BodyModel[0].x * ModelsValue[1][0].x + BodyModel[3].x;
-    float BodybottomMaxx = BodyModel[0].x * ModelsValue[1][2].x + BodyModel[3].x;
-    float BodybottomMinz = BodyModel[2].z * ModelsValue[1][2].z + BodyModel[3].z;
-    float BodybottomMaxz = BodyModel[2].z * ModelsValue[1][0].z + BodyModel[3].z;
-    
-    
-    //탱크 왼쪽 팔 부분
-    glBindVertexArray(VAO[2]);
-    glm::mat4 LeftArmModel = glm::mat4(1.0f);
-    LeftArmModel = glm::translate(LeftArmModel, glm::vec3(EraserTransSize.AllTransX, EraserTransSize.AllTransY, EraserTransSize.AllTransZ));
-    LeftArmModel = glm::translate(LeftArmModel, glm::vec3(0.0f, -0.25f, 0.0f));
-    LeftArmModel = glm::scale(LeftArmModel, glm::vec3(0.5f, 0.5f, 0.5f));
-    unsigned int LeftArmModelLocation = glGetUniformLocation(shaderID[1], "modeltransform");
-    glUniformMatrix4fv(LeftArmModelLocation, 1, GL_FALSE, glm::value_ptr(LeftArmModel));
-    unsigned int LeftArmModelFragLocation = glGetUniformLocation(shaderID[1], "vColor");
-    glUniform3f(LeftArmModelFragLocation, 0.75f, 0.75f, 0.75f);
-    glDrawArrays(GL_QUADS, 0, 24);
-    
-    //지워질 아이들
-    if (randomExist) {
-        for (int randomtrans = 0; randomtrans < ModelNumber; randomtrans++) {
-            randomExistSquare(randomtrans);
+    //아래-기본 y 스케일, 색깔 부여
+    if (OnceMakeColor) {
+        for (int i = 0; i < ModelCountCol; i++) {
+            for (int j = 0; j < ModelCountRow; j++) {
+                makeModelColor(i, j);
+                randomScale(i, j);
+                ScaleCount[i][j] = 0;
+            }
         }
     }
-    randomExist = false;
-    //충돌처리 곧 다감
+    OnceMakeColor = false;
 
-    for (int ModelCountNumber = 0; ModelCountNumber < ModelNumber; ModelCountNumber++) {        
-        
-        glBindVertexArray(VAO[ModelCountNumber + 3]);
-        glm::mat4 ExistSqure = glm::mat4(1.0f);
-        ExistSqure = glm::translate(ExistSqure, glm::vec3(ModelTransX[ModelCountNumber], 0.0f, ModelTransZ[ModelCountNumber]));
-        ExistSqure = glm::scale(ExistSqure, 
-            glm::vec3(ChangeScaleSquqre[ModelCountNumber].x, ChangeScaleSquqre[ModelCountNumber].y, ChangeScaleSquqre[ModelCountNumber].z));
-
-        float SquareMinX = ModelsValue[ModelCountNumber + 3][0].x * ExistSqure[0].x + ExistSqure[3].x;
-        float SquareMaxX = ModelsValue[ModelCountNumber + 3][2].x * ExistSqure[0].x + ExistSqure[3].x;
-        float SquareMinZ = ModelsValue[ModelCountNumber + 3][2].z * ExistSqure[2].z + ExistSqure[3].z;
-        float SquareMaxZ = ModelsValue[ModelCountNumber + 3][0].z * ExistSqure[2].z + ExistSqure[3].z;
-       
-        //충돌 이따가 다시 
-        if ((((BodybottomMinx <= SquareMinX) && (SquareMinX <= BodybottomMaxx)) && ((BodybottomMinx <= SquareMaxX) && (SquareMaxX <= BodybottomMaxx)))
-            && (((BodybottomMinz <= SquareMinZ) && (SquareMinZ <= BodybottomMaxz)) && ((BodybottomMinz <= SquareMinZ) && (SquareMinZ <= BodybottomMaxz)))) {
-            swap(ModelTransX[ModelCountNumber], ModelTransX[ModelNumber - 1]);
-            swap(ModelTransZ[ModelCountNumber], ModelTransZ[ModelNumber - 1]);
-            swap(ChangeScaleSquqre[ModelCountNumber], ChangeScaleSquqre[ModelNumber - 1]);
-            smallscale[ModelNumber - 1] = true;
-            ChangeSmallC = ModelNumber - 1;
-        } 
-		unsigned int ExistSqureLocation = glGetUniformLocation(shaderID[1], "modeltransform");
-		glUniformMatrix4fv(ExistSqureLocation, 1, GL_FALSE, glm::value_ptr(ExistSqure));
-		unsigned int ExistSqureFragLocation = glGetUniformLocation(shaderID[1], "vColor");
-		glUniform3f(ExistSqureFragLocation, 0.1f, 0.5f, 0.2f);
-		glDrawArrays(GL_QUADS, 0, 24);
+    //시작점 구함
+    float PrimeX = ModelsValue[0][0].x * PlaneModel[0].x;
+    float PrimeZ = ModelsValue[0][55].z * PlaneModel[2].z;
+    //glEnable(GL_CULL_FACE);
+    for (int i = 0; i < ModelCountCol; i++) {
+        for (int j = 0; j < ModelCountRow; j++) {            
+            glBindVertexArray(VAO[1]);
+            glm::mat4 RailModel = glm::mat4(1.0f);
+            RailModel = glm::translate(RailModel, glm::vec3(PrimeX + 0.5 * i + 0.25f, 0.0f, PrimeZ + 0.5 * j + 0.25f));
+            RailModel = glm::scale(RailModel, glm::vec3(0.5f, MoutainScale[i][j], 0.5f));        
+            unsigned int RailModelLocation = glGetUniformLocation(shaderID[1], "modeltransform");
+            glUniformMatrix4fv(RailModelLocation, 1, GL_FALSE, glm::value_ptr(RailModel));
+            unsigned int RailModelFragLocation = glGetUniformLocation(shaderID[1], "vColor");
+            glUniform3f(RailModelFragLocation, ModelColorR[i][j], ModelColorG[i][j], ModelColorB[i][j]);
+            glDrawArrays(GL_QUADS, 0, 24);
+        }
     }
 }
+void makeModelColor(int i, int j) {
+    random_device rd;
+    default_random_engine dre(rd());
+    uniform_real_distribution<>uid(0.0, 1.0);
+    float mR = uid(dre);
+    float mG = uid(dre);
+    float mB = uid(dre);
+    ModelColorR[i][j] = mR;
+    ModelColorG[i][j] = mG;
+    ModelColorB[i][j] = mB;
+}
+void randomScale(int i,int j) {
+    random_device rd;
+    default_random_engine dre(rd());
+    uniform_real_distribution<>uid(0.5, 3.5);
+    float SY = uid(dre);
+    MoutainScale[i][j] = SY;
+}
+
 GLvoid drawscene() {
     glClearColor(0.0, 0.0, 0.0, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -310,16 +290,19 @@ GLvoid drawscene() {
     initbuffer();
 
     glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
 
     glUseProgram(shaderID[1]);
+
+    //메인뷰
+    glViewport(0, 0, windowWidth, windowHeight);
 
     unsigned int ModelViewLocation = glGetUniformLocation(shaderID[1], "viewtransform");
     unsigned int ModelProjLocation = glGetUniformLocation(shaderID[1], "projectiontransform");
 
     glm::vec3 ModelCameraPos = glm::vec3(CameraPoint.ViewX, CameraPoint.ViewY, CameraPoint.ViewZ); // EYE
     glm::mat4 CameraPosDistance = glm::mat4(1.0f);
-    CameraPosDistance = glm::rotate(CameraPosDistance, glm::radians(ObjectTheta), glm::vec3(0.0f, 1.0f, 0.0f));
+    CameraPosDistance = glm::rotate(CameraPosDistance, glm::radians(ObjectThetaX), glm::vec3(1.0f, 0.0f, 0.0f));
+    CameraPosDistance = glm::rotate(CameraPosDistance, glm::radians(ObjectThetaY), glm::vec3(0.0f, 1.0f, 0.0f));
     glm::vec4 CameraPosDis = glm::vec4(ModelCameraPos, 1);
     CameraPosDis = CameraPosDistance * CameraPosDis;
     glm::vec3 CameraPostionDirection = glm::vec3(CameraPosDis.x, CameraPosDis.y, CameraPosDis.z);  //EYE 변환
@@ -345,19 +328,50 @@ GLvoid drawscene() {
     CenterDirection = CenterDirectionDistance * CenterDirection;
     CenterDirectionDis = glm::vec3(CenterDirection.x + CameraPostionDirection.x,
         CenterDirection.y + CameraPostionDirection.y, CenterDirection.z + CameraPostionDirection.z); //n
+    
+    glm::vec3  ModelCameraRight; //v
+    if ((90 <= (abs((int)ObjectThetaX)) % 360) && ((abs((int)ObjectThetaX)) % 360 <= 270)) {
+        ModelCameraRight = glm::normalize(glm::cross(CameraDirection, up)); //v
+    }
+    else {
+        ModelCameraRight = glm::normalize(glm::cross(up, CameraDirection));
+    }
 
-    glm::vec3 ModelCameraRight = glm::normalize(glm::cross(up, CameraDirection)); //v
+
 
     glm::vec3 cameraUp = glm::cross(CameraDirection, ModelCameraRight); //u
-
     glm::mat4 ModelView = glm::mat4(1.0f);
     ModelView = glm::lookAt(CameraPostionDirection, CenterDirectionDis, cameraUp);
     glUniformMatrix4fv(ModelViewLocation, 1, GL_FALSE, glm::value_ptr(ModelView));
+
 
     //원근
     glm::mat4 ModelProj = glm::mat4(1.0f);
     ModelProj = glm::perspective(glm::radians(60.0f), (float)windowWidth / windowHeight, 0.1f, 100.0f);
     glUniformMatrix4fv(ModelProjLocation, 1, GL_FALSE, &ModelProj[0][0]);
+
+    drawmodels();
+
+    //탑뷰
+    glViewport(windowWidth * 3 / 4, windowHeight * 3 / 4, windowWidth / 4, windowHeight / 4);
+
+    unsigned int TopViewLocation = glGetUniformLocation(shaderID[1], "viewtransform");
+
+    glm::vec3 TopCameraPos = glm::vec3(0.0f, 4.0f, 0.0f); // EYE
+
+    glm::vec3 TopCameraTarget = glm::vec3(0.0f, 0.0f, 0.0f); //--- 카메라 바라보는 방향
+    glm::vec3 TopCameraDirection = TopCameraTarget - TopCameraPos;
+
+    glm::vec3 TopViewUp = glm::vec3(0.0f, 0.0f, -1.0f);
+
+    glm::mat4 TopView = glm::mat4(1.0f);
+    TopView = glm::lookAt(TopCameraPos, TopCameraDirection, TopViewUp);
+    glUniformMatrix4fv(TopViewLocation, 1, GL_FALSE, glm::value_ptr(TopView));
+
+    glm::mat4 TopOrtho = glm::mat4(1.0f);
+    TopOrtho = glm::ortho(-2.0f, 2.0f, -2.0f, 2.0f, 0.7f, 20.0f);
+    unsigned int TopOrthoLocation = glGetUniformLocation(shaderID[1], "projectiontransform");
+    glUniformMatrix4fv(TopOrthoLocation, 1, GL_FALSE, glm::value_ptr(TopOrtho)); //직각투영
 
     drawmodels();
 
@@ -376,9 +390,9 @@ void resize(int width, int height) {
 
 void initbuffer() {
 
-    glGenVertexArrays(ModelNumber+3, VAO);
+    glGenVertexArrays(2, VAO);
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 2; i++) {
         glBindVertexArray(VAO[i]);
         glGenBuffers(1, &VBO[i]);
         glBindBuffer(GL_ARRAY_BUFFER, VBO[i]);
@@ -386,38 +400,31 @@ void initbuffer() {
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec4), (void*)0);
         glEnableVertexAttribArray(0);
     }
-	for (int j = 0; j < ModelNumber; j++) {
-		glBindVertexArray(VAO[j + 3]);
-		glGenBuffers(1, &VBO[j + 3]);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO[j + 3]);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec4) * ModelsValue[j + 3].size(), &ModelsValue[j + 3][0], GL_STREAM_DRAW);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec4), (void*)0);
-		glEnableVertexAttribArray(0);
-	}
+
 }
 
 GLvoid keyboard(unsigned char key, int x, int y) {
     switch (key)
     {
     case 'c': //초기화
-        CameraPoint.ViewZ = 5.0f;
-        CameraPoint.ViewX = 0.0f;
-        CameraPoint.ViewY = 1.0f;
-        memset(&EraserTransSize, 0, sizeof(EraserTransSize));
-        for (int i = 0; i < 30; i++) {
-            ChangeScaleSquqre[i] = { 0.25f };
-        }
-        memset(&ModelTransX, 0.0f, sizeof(ModelTransX));
-        memset(&ModelTransZ, 0.0f, sizeof(ModelTransZ));
-        randomExist = true;
+        MoutainScaleOn = false;
+        OnceMakeColor = true;
+        ObjectThetaX = -20.0f;
+        ObjectThetaY = 0.0f;
         break;
         //카메라 방향
     case 'x':
-        CameraPoint.ViewX += 0.15f;
+        ViewTransX = !ViewTransX;
         break;
     case 'X':
         CameraPoint.ViewX -= 0.15f;
         break;
+    case 'y':
+        ViewTransY = !ViewTransY;
+        ObjectThetaX = 0.0f;
+        break;
+    case 'Y':
+        
         break;
     case 'z':
         CameraPoint.ViewZ += 0.15f;
@@ -425,19 +432,11 @@ GLvoid keyboard(unsigned char key, int x, int y) {
     case 'Z':
         CameraPoint.ViewZ -= 0.15f;
         break;
-
-//오브젝트 옮기기
-    case 'w':
-        EraserTransSize.AllTransZ -= 0.1f;
+    case 't':
+        MoutainScaleOn = !MoutainScaleOn;
         break;
-    case 's':
-        EraserTransSize.AllTransZ += 0.1f;
-        break;
-    case 'a':
-        EraserTransSize.AllTransX -= 0.1f;
-        break;
-    case 'd':
-        EraserTransSize.AllTransX += 0.1f;
+    case 'k':
+        
         break;
     case 'q':
         exit(0);
@@ -450,16 +449,33 @@ GLvoid keyboard(unsigned char key, int x, int y) {
 }
 
 void timer(int value) {
-    if (smallscale[ChangeSmallC]) {
-        if (ChangeScaleSquqre[ChangeSmallC].x >= 0) {
-            ChangeScaleSquqre[ChangeSmallC].x -= 0.02f;
-            ChangeScaleSquqre[ChangeSmallC].y -= 0.02f;
-            ChangeScaleSquqre[ChangeSmallC].z -= 0.02f;
-        }
-        else {
-            smallscale[ChangeSmallC] = false;
+    if (MoutainScaleOn) {
+        for (int i = 0; i < COL; i++) {
+            for (int j = 0; j < ROW; j++) {
+                if (MoutainScale[i][j] >= 8.0) {
+                    ScaleCount[i][j] ++;
+                }
+                else if (MoutainScale[i][j] <= 0.3) {
+                    ScaleCount[i][j]--;
+                }
+
+                if (ScaleCount[i][j] == 0) {
+                    MoutainScale[i][j] += 0.2f;
+                }
+                else {
+                    MoutainScale[i][j] -= 0.2f;
+                }
+            }
         }
     }
+    if (ViewTransX) {
+        ObjectThetaX += 1.0f;
+    }
+    if (ViewTransY) {
+        ObjectThetaY += 1.0f;
+    }
+
+
     glutPostRedisplay();
-    glutTimerFunc(60, timer, 1);
+    glutTimerFunc(1, timer, 1);
 }
